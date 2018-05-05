ea110c4da6056e3a9f9a64da45f4576b0dfd5c48
artlenk.ru@gmail.com <df5ukoi7ht>
2018-05-05 22:03:41 +0300

    Fixed tvs catalog

---
+++ src/js/common.js
@@ -0,0 +1,29 @@
+/*
+$(document).ready(function () {
+    $('.presentation-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        arrows: false
+    });
+
+    var menuOpen = 'menu-open';
+    $('.menu-toggle').click(
+        function () {
+            $('body').addClass(menuOpen);
+        }
+    );
+
+    $('.menu-close').click(
+        function () {
+            $('body').removeClass(menuOpen);
+        }
+    );
+});
+
+//initiating jQuery
+jQuery(function ($) {
+    $(document).ready(function () {
+        $('.menu-inner').stickUp();
+    });
+});
+*/
---
+++ src/js/lib.js
@@ -0,0 +1,11 @@
+/*
+ * Content of libraries
+ * 1.   smoothscroll.js
+ * 2.   bootstrap.min.js
+ */
+
+/* 1. smoothscroll.js */
+//= lib/smoothscroll.js
+
+/* 2. bootstrap.min.js */
+//= lib/bootstrap.min.js
