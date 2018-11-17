bcd343f0449a5903346302090899a9a275aa41a4
alex-lenk <artlenk.ru@gmail.com>
2018-11-17 17:30:23 +0300

    Fixed minor bugs

--- src/js/lib.js
+++ src/js/lib.js
@@ -2,6 +2,7 @@
  * Content of libraries
  * 1.   smoothscroll.js
  * 2.   bootstrap.min.js
+ * 3.   jquery.fancybox.min.js
  */
 
 /* 1. smoothscroll.js */
@@ -9,3 +10,6 @@
 
 /* 2. bootstrap.min.js */
 //= lib/bootstrap.min.js
+
+/* 3. jquery.fancybox.min.js */
+//= lib/jquery.fancybox.min.js
