4bea9df2b1de98aa22c82e4b7c02bd0895810c46
AlexLenk <artlenk.ru@gmail.com>
2019-09-30 23:04:09 +0300

    Refactoring js scripts

---
+++ src/js/general.js
@@ -0,0 +1,11 @@
+/*
+ * Content of libraries
+ * 1.   jquery.min.js
+ * 2.   jquery.fancybox.min.js
+ */
+
+/* 1. jquery.min.js */
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/* 2. jquery.fancybox.min.js */
+//= lib/jquery.fancybox.min.js
