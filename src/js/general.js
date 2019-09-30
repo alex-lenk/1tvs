68a40ca1c1e68ed93a56bd6ad39bbfa9bfec5faa
AlexLenk <artlenk.ru@gmail.com>
2019-09-30 23:56:33 +0300

    Added new functionality

--- src/js/general.js
+++ src/js/general.js
@@ -2,6 +2,8 @@
  * Content of libraries
  * 1.   jquery.min.js
  * 2.   jquery.fancybox.min.js
+ * 3.   jquery.maskedinput.min.js
+ * 3.   scripts.js
  */
 
 /* 1. jquery.min.js */
@@ -9,3 +11,9 @@
 
 /* 2. jquery.fancybox.min.js */
 //= lib/jquery.fancybox.min.js
+
+/* 3. jquery.maskedinput.min.js */
+//= lib/jquery.maskedinput.min.js
+
+/* 4. scripts.js */
+//= scripts.js
